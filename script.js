1edfe7330044682496d24dc30f878c4d8f0cb549
Josepascal <Josepascal@users.noreply.github.com>
2022-03-03 21:48:14 -0500

    add function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const computerPlay = () => {
-  console.log("Rock");
+  console.log("rock");
 }
+
+const playRound = (playerSelection, computerSelection) => {
+  let player = playerSelection.toLowerCase();
+
+  if (player === 'rock' && computerSelection === 'paper') {
+    return 'You lose! Paper beats Rock';
+  } else if (player === 'scissors' && computerSelection === 'rock') {
+    return 'You lose! Rock beats Scissors';
+  } else if (player === 'paper' && computerSelection === 'scissors') {
+    return 'You lose! Scissors beats Paper';
+  } else {
+    return 'You Win!!';
+  }
+}
+
+// const playerSelection = "scissors";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
