c3ddfa190c11457d2c4fd02f283deccbbd8a7fc7
Josepascal <Josepascal@users.noreply.github.com>
2022-03-04 22:19:01 -0500

    add play round function

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+// Return radomly rock, paper, or scissors
 const computerPlay = () => {
-  console.log("rock");
+  const selection = ["rock", "paper", "scissors"];
+  let randomIndex = Math.round(Math.random() * 2);
+  return selection[randomIndex];
 }
 
+// Function that plays one round of game
 const playRound = (playerSelection, computerSelection) => {
   let player = playerSelection.toLowerCase();
 
-  if (player === 'rock' && computerSelection === 'paper') {
-    return 'You lose! Paper beats Rock';
-  } else if (player === 'scissors' && computerSelection === 'rock') {
-    return 'You lose! Rock beats Scissors';
-  } else if (player === 'paper' && computerSelection === 'scissors') {
-    return 'You lose! Scissors beats Paper';
-  } else {
-    return 'You Win!!';
-  }
+  if (computerSelection === 'paper' && player === 'rock') return 'You lose! Paper beats Rock';
+  if (computerSelection === 'scissors' && player === 'paper') return 'You lose! Scissors beats Paper';
+  if (computerSelection === 'rock' && player === 'scissors') return 'You lose! Rock beats Scissors';
+  if (computerSelection === player) return 'It is a tie!';
+
+  return 'You Win!!';
 }
 
-// const playerSelection = "scissors";
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
